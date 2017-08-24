936c836f8b01f72d9b20ff0bcdf1f0177c70584f
Philipp Klaus <klaus@physik.uni-frankfurt.de>
2017-08-24 11:37:50 +0200

    DASH: updateSparklines() -> /static/js/script.js

--- static/js/script.js
+++ static/js/script.js
@@ -2,3 +2,21 @@
 alert("whats up");
 console.log("what's up?");
 */
+
+function updateSparklines() {
+  var sparklineItems = document.getElementsByClassName("sparkline");
+  [].forEach.call(sparklineItems, function (el) {
+    var el_id = el.id;
+    var pvName = el_id.substring(el_id.indexOf("-")+1, el_id.length);
+    var pvName = pvName.replace(/-/g, ':');
+
+    var sparklineChart = sparkline()
+                         .width(155)
+                         .height(50)
+                         .gradientColors(['green', 'orange', 'red']) // top -> bottom
+                         .dataSource('/api/history/' + pvName + '.json')
+                         .dataSourceType('JSON')
+                         .selector('#'+el.id);
+    sparklineChart();  // render the chart
+  });
+};

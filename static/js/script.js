4287e88236b89c0ed932ea7a15216ce1fe062e4a
Philipp Klaus <philipp.klaus@gmail.com>
2017-09-23 11:24:24 +0800

    DASH: move common style/js into static files

--- static/js/script.js
+++ static/js/script.js
@@ -20,3 +20,33 @@ function updateSparklines() {
     sparklineChart();  // render the chart
   });
 };
+
+
+function copyToClipboard(text, el) {
+  // COPY TO CLIPBOARD
+  // Attempts to use .execCommand('copy') on a created text field
+  // Falls back to a selectable alert if not supported
+  // Attempts to display status in Bootstrap tooltip
+  // ------------------------------------------------------------------------------
+  var copyTest = document.queryCommandSupported('copy');
+  var elOriginalText = el.attr('data-original-title');
+
+  if (copyTest === true) {
+    var copyTextArea = document.createElement("textarea");
+    copyTextArea.value = text;
+    document.body.appendChild(copyTextArea);
+    copyTextArea.select();
+    try {
+      var successful = document.execCommand('copy');
+      var msg = successful ? 'Copied!' : 'Whoops, not copied!';
+      el.attr('data-original-title', msg).tooltip('show');
+    } catch (err) {
+      console.log('Oops, unable to copy');
+    }
+    document.body.removeChild(copyTextArea);
+    el.attr('data-original-title', elOriginalText);
+  } else {
+    // Fallback if browser doesn't support .execCommand('copy')
+    window.prompt("Copy to clipboard: Ctrl+C or Command+C, Enter", text);
+  }
+}
